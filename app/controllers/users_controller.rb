14c88c625415bc82a3b0b0d2a8cb4306daa23528
Marlena Tsang <Marlena345@gmail.com>
2022-03-08 10:20:32 -0500

    Completed Assignment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,6 @@
 class UsersController < ApplicationController
+  def show
+    users = User.find_by(id: params[:id])
+    render json: users, except: [:created_at, :updated_at], include: [:items]
+  end
 end
